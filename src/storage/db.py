48d450463a616b6840f3469d3018c74f6cbc53d2
Joseph Peters <josephpetersind@gmail.com>
2026-07-04 16:31:46 +0530

    feat: impement SQLite database storage layer

---
+++ src/storage/db.py
@@ -0,0 +1,51 @@
+import sqlite3
+import time
+from pathlib import Path
+
+DB_PATH = Path("data/tracker.db")
+
+def initialize_db():
+    """creates the database file and sessions table if they don't exist"""
+
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute('''
+                       CREATE TABLE IF NOT EXISTS sessions (
+                           id INTEGER PRIMARY KEY AUTOINCREMENT,
+                           application TEXT NOT NULL,
+                           start_time REAL NOT NULL,
+                           end_time REAL NOT NULL,
+                           duration_seconds REAL NOT NULL
+                )
+        ''')
+
+        conn.commit()
+        print("Database initialized successfully.")
+
+def save_session(app_name, start_time, end_time):
+    """saves a session record to the database"""
+
+    duration = end_time - start_time
+
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute('''
+                       INSERT INTO sessions (application, start_time, end_time, duration_seconds)
+                       VALUES (?, ?, ?, ?)
+                ''', (app_name, start_time, end_time, duration))
+
+        conn.commit()
+        print(f"Saved session: [{app_name}] for duration {duration:.2f} seconds")
+
+if __name__ == "__main__":
+    print("testing database module...")
+
+    initialize_db()
+
+    fake_end_time = time.time()
+    fake_start_time = fake_end_time - 120  # Simulate a session of 2 minutes
+
+    save_session("TestApp", fake_start_time, fake_end_time)
+    
